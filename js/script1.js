96aaad264b7718b149067d7cc74b33d35f355c4c
faustinopieroni1 <fauspieroni@gmail.com>
2022-10-03 19:11:13 -0300

    corregi errores primera entrega

--- js/script1.js
+++ js/script1.js
@@ -8,7 +8,7 @@ let nombre = prompt("Ingrese su nombre de usuario")
 let contraseña = prompt("Ingrese su contraseña")
 
 
-if ((nombre !="") && (contraseña !="")) {
+if ((nombre != "") && (contraseña != "")) {
 
 
 
@@ -38,68 +38,63 @@ if ((nombre !="") && (contraseña !="")) {
 
     let opcion = prompt("Bienvenido a Like Nutrition.cba\nENTER para acceder a Suplementos \nESC para finalizar compra")
 
-    if (opcion == "1") {
-        while (opcion != "ESC") {
-            //1-Mostrar productos
-            opcion = prompt("Agregar al carrito: \n1- Whey protein truemade 1kg\n2- Creatina micronizada ENA 300gr\n3- Ultra mass ENA 1500gr\nESC- Para salir")
 
-            //2-Cantidad de productos
-            if ((opcion == "Whey protein truemade 1kg") || (opcion == "1")) {
-                let cantidadProductoA = prompt("Ingrese cantidad de: " + productoA + " que desea comprar")
-                //3-Calculo de precios
-                //4-control de stock 
-                if (cantidadProductoA <= stockProductoA) {
-                    precioTotal = precioTotal + (cantidadProductoA * precioProductoA) //-> Sumo el nuevo precio total al viejo precio total para que los valores se vayan sumando 
-                }
-                else {
-                    alert("Actualmente tenemos: " + stockProductoA + " unidades disponibles")
-                }
+    while (opcion != "ESC") {
+        //1-Mostrar productos
+        opcion = prompt("Agregar al carrito: \n1- Whey protein truemade 1kg\n2- Creatina micronizada ENA 300gr\n3- Ultra mass ENA 1500gr\nESC- Para salir")
+
+        //2-Cantidad de productos
+        if ((opcion == "Whey protein truemade 1kg") || (opcion == "1")) {
+            let cantidadProductoA = prompt("Ingrese cantidad de: " + productoA + " que desea comprar")
+            //3-Calculo de precios
+            //4-control de stock 
+            if (cantidadProductoA <= stockProductoA) {
+                precioTotal = precioTotal + (cantidadProductoA * precioProductoA) //-> Sumo el nuevo precio total al viejo precio total para que los valores se vayan sumando 
             }
-
-            //2-Cantidad de productos
-            else if ((opcion == "Creatina micronizada ENA 300gr") || (opcion == "2")) {
-                let cantidadProductoB = prompt("Ingrese cantidad de: " + productoB + " que desea comprar")
-                //3-Calculo de precios
-                //4-control de stock 
-                if (cantidadProductoB <= stockProductoB) {
-                    precioTotal = precioTotal + (cantidadProductoB * precioProductoB)
-                }
-                else {
-                    alert("Actualmente tenemos: " + stockProductoB + " unidades disponibles")
-                }
-            }
-
-            //2-Cantidad de productos
-            else if ((opcion == "Ultra mass ENA 1500g") || (opcion == "3")) {
-                let cantidadProductoC = prompt("Ingrese cantidad de: " + productoC + " que desea comprar")
-                //3-Calculo de precios
-                //4-control de stock 
-                if (cantidadProductoC <= stockProductoC) {
-                    precioTotal = precioTotal + (cantidadProductoC * precioProductoC)
-                }
-                else {
-                    alert("Actualmente tenemos: " + stockProductoC + " unidades disponibles")
-                }
+            else {
+                alert("Actualmente tenemos: " + stockProductoA + " unidades disponibles")
             }
         }
 
-        //5- Si hay stock se va a mostrar el precio
-        if (precioTotal != 0) {
-            alert("El precio total es de: " + precioTotal)//-> Uso de la variable glogal que declare al principo y adquirio valores en el bucle 
+        //2-Cantidad de productos
+        else if ((opcion == "Creatina micronizada ENA 300gr") || (opcion == "2")) {
+            let cantidadProductoB = prompt("Ingrese cantidad de: " + productoB + " que desea comprar")
+            //3-Calculo de precios
+            //4-control de stock 
+            if (cantidadProductoB <= stockProductoB) {
+                precioTotal = precioTotal + (cantidadProductoB * precioProductoB)
+            }
+            else {
+                alert("Actualmente tenemos: " + stockProductoB + " unidades disponibles")
+            }
         }
 
-        //6-mensajes post servicio
-        if (precioTotal != 0) {
-            alert("¡Gracias por su compra!")
-        }
-        if (precioTotal == 0) {
-            alert("¡Vuelva Pronto!")
+        //2-Cantidad de productos
+        else if ((opcion == "Ultra mass ENA 1500g") || (opcion == "3")) {
+            let cantidadProductoC = prompt("Ingrese cantidad de: " + productoC + " que desea comprar")
+            //3-Calculo de precios
+            //4-control de stock 
+            if (cantidadProductoC <= stockProductoC) {
+                precioTotal = precioTotal + (cantidadProductoC * precioProductoC)
+            }
+            else {
+                alert("Actualmente tenemos: " + stockProductoC + " unidades disponibles")
+            }
         }
     }
 
-    
-}
+    //5- Si hay stock se va a mostrar el precio
+    if (precioTotal != 0) {
+        alert("El precio total es de: " + precioTotal)//-> Uso de la variable glogal que declare al principo y adquirio valores en el bucle 
 
+        //6-mensajes post servicio
+        alert("¡Gracias por su compra!")
+    }
+    else {
+        alert("¡Vuelva Pronto!")
+    }
+
+}
 else {
-    alert("Error, intente nuevamente")
+    alert("Ingrese nombre de usuario y contraseña validos")
 }
