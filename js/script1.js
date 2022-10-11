be8d4c4322eb214d412b7f7e7cdd050a22080907
faustinopieroni1 <fauspieroni@gmail.com>
2022-10-10 22:22:50 -0300

    agregamos funciones, objetos, fn constructoras de objetos y distintos tipos de metodos con funciones de orden superior

--- js/script1.js
+++ js/script1.js
@@ -1,6 +1,6 @@
 //Login
-// Nombre: usuario
-// Contraseña: usuario
+// Nombre !=""
+// Contraseña !="" 
 
 alert("Bienvenido, a continuacion ingrese su usuario")
 
@@ -22,63 +22,83 @@ if ((nombre != "") && (contraseña != "")) {
     //6-mensajes post servicio.
 
 
-    let productoA = "Whey protein truemade 1kg"
-    let precioProductoA = 200
-    let stockProductoA = 10
 
-    let productoB = "Creatina micronizada ENA 300gr"
-    let precioProductoB = 100
-    let stockProductoB = 10
+    //Uso de funcion Constructora de objetos-------------
 
-    let productoC = "Ultra mass ENA 1500gr"
-    let precioProductoC = 50
-    let stockProductoC = 10
+    function Producto(nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock
+    }
+
+    let productoA = new Producto("1- Whey protein truemade 1kg", 200, 10);
+    let productoB = new Producto("2- Creatina micronizada ENA 300gr", 100, 10);    //--->Objetos construidos
+    let productoC = new Producto("3- Ultra mass ENA 1500gr", 50, 10);
+    let productoD = new Producto("4- Preentreno C4 300gr", 70, 0);
+
+    //USO DE ARRAYS + METODOS--------------
+
+    let productos = [productoA, productoB, productoC, productoD]    // ---> Lista de objetos (Array)
+
+    let listaProductosConStock = productos.filter((parametro) => parametro.stock > 0)  // Array solo tendra productos  c/stock, gracias a metodo .filter()
+
+    let listaProductos = listaProductosConStock.map((parametro) => parametro.nombre)// Array tendra propiedad .nombre de "listaProductos" x el metodo .map()
+
+    //-------------------------------------
 
     let precioTotal = 0 //-> Asigno la variable afuera del ciclo para poder utilizarla dentro de el y para poder almacenar valores dentro de ella y citarla fuera del ciclo.
+
+    //USO DE FUNCION-----------------------
+
+    function calculoPrecio(cantidad, precio) { // Decalracion de funcion con parametros
+        precioTotal += (cantidad * precio) // El "+=" sirve para sumarle un nuevo valor al valor actual de la variable y se va sobre escribiendo
+    }
+
+    //-------------------------------------
 
     let opcion = prompt("Bienvenido a Like Nutrition.cba\nENTER para acceder a Suplementos \nESC para finalizar compra")
 
 
     while (opcion != "ESC") {
         //1-Mostrar productos
-        opcion = prompt("Agregar al carrito: \n1- Whey protein truemade 1kg\n2- Creatina micronizada ENA 300gr\n3- Ultra mass ENA 1500gr\nESC- Para salir")
+        opcion = prompt("Agregar al carrito: \n" + listaProductos.join("\n") + "\n-ESC para avanzar")  //Uso del metodo .join()
 
         //2-Cantidad de productos
         if ((opcion == "Whey protein truemade 1kg") || (opcion == "1")) {
-            let cantidadProductoA = prompt("Ingrese cantidad de: " + productoA + " que desea comprar")
+            let cantidadProductoA = prompt("Ingrese cantidad de: " + productoA.nombre + " que desea comprar")
             //3-Calculo de precios
             //4-control de stock 
-            if (cantidadProductoA <= stockProductoA) {
-                precioTotal = precioTotal + (cantidadProductoA * precioProductoA) //-> Sumo el nuevo precio total al viejo precio total para que los valores se vayan sumando 
+            if (cantidadProductoA <= productoA.stock) {
+                precioTotal = precioTotal + (cantidadProductoA * productoA.precio) //-> Sumo el nuevo precio total al viejo precio total para que los valores se vayan sumando 
             }
             else {
-                alert("Actualmente tenemos: " + stockProductoA + " unidades disponibles")
+                alert("Actualmente tenemos: " + productoA.stock + " unidades disponibles")
             }
         }
 
         //2-Cantidad de productos
         else if ((opcion == "Creatina micronizada ENA 300gr") || (opcion == "2")) {
-            let cantidadProductoB = prompt("Ingrese cantidad de: " + productoB + " que desea comprar")
+            let cantidadProductoB = prompt("Ingrese cantidad de: " + productoB.nombre + " que desea comprar")
             //3-Calculo de precios
             //4-control de stock 
-            if (cantidadProductoB <= stockProductoB) {
-                precioTotal = precioTotal + (cantidadProductoB * precioProductoB)
+            if (cantidadProductoB <= productoB.stock) {
+                precioTotal = precioTotal + (cantidadProductoB * productoB.precio)
             }
             else {
-                alert("Actualmente tenemos: " + stockProductoB + " unidades disponibles")
+                alert("Actualmente tenemos: " + productoB.stock + " unidades disponibles")
             }
         }
 
         //2-Cantidad de productos
         else if ((opcion == "Ultra mass ENA 1500g") || (opcion == "3")) {
-            let cantidadProductoC = prompt("Ingrese cantidad de: " + productoC + " que desea comprar")
+            let cantidadProductoC = prompt("Ingrese cantidad de: " + productoC.nombre + " que desea comprar")
             //3-Calculo de precios
             //4-control de stock 
-            if (cantidadProductoC <= stockProductoC) {
-                precioTotal = precioTotal + (cantidadProductoC * precioProductoC)
+            if (cantidadProductoC <= productoC.stock) {
+                precioTotal = precioTotal + (cantidadProductoC * productoC.precio)
             }
             else {
-                alert("Actualmente tenemos: " + stockProductoC + " unidades disponibles")
+                alert("Actualmente tenemos: " + productoC.stock + " unidades disponibles")
             }
         }
     }
